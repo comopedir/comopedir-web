174f40872cdff500ba76c575b67f7c6ad62d9e46
Gabriel Kozlowaski <gkm.kozlowaski@gmail.com>
2020-05-02 16:47:32 -0300

    Implement FAQItem component

---
+++ src/components/FAQ/FAQItem/FAQItem.stories.js
@@ -0,0 +1,12 @@
+import React from "react"
+import FAQItem from "./index"
+
+export default {
+  title: "FAQ/FAQItem",
+}
+
+export const standard = () => (
+  <FAQItem question="Como posso incluir o meu estabelecimento?">
+    Fácil, clique em <a href="/">‘Cadastrar.’</a>
+  </FAQItem>
+)
